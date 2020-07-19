552039504b75ec97ae13416441cbb023f3dd8883
Hamza Akram <hamza.akram4635@gmail.com>
2020-07-20 00:20:05 +0500

    added incomeExpense component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Header } from "./components/header";
 import { Balance } from "./components/balance";
+import { IncomeExpenses } from "./components/incomeExpenses";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <div className="container">
         <Balance />
+        <IncomeExpenses />
       </div>
     </div>
   );
